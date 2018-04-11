f182706231c413dc44b869836618bf628bca897b
Erik Seifert <erik.seifert@b-connect.de>
2018-04-11 13:12:15 +0200

    Fix: add listener

--- dist/js/plugin-twig-doc.js
+++ dist/js/plugin-twig-doc.js
@@ -1,15 +1,21 @@
 
 var TwigDocPlugin = {
 
-	insert: function(patternData) {
-
-		console.log("trying to insert");
-		console.log(patternData.extraOutput);
-
+	insert: function(panels, patternData, iframePassback, switchText) {
+    $(panels).find('#sg-atoms-colors-pl-panel-variables-panel').html('HERE');
   },
 
-  setup: function() {
-    console.log('Setup panels');
-  }
-
 }
+
+Panels.add({
+	'id': 'pl-panel-variables',
+	'name': 'Variables',
+	'default': false,
+	'templateID': 'pl-panel-template-code',
+	'httpRequest': true,
+	'httpRequestReplace': fileSuffixMarkup + '.html',
+	'httpRequestCompleted': false,
+	'prismHighlight': true,
+	'language': 'markup',
+  'keyCombo': 'ctrl+shift+y'
+})
