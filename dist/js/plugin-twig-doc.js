3591b9439e8ad72f93751376598c6aa894a8ad31
Erik Seifert <erik.seifert@b-connect.de>
2018-04-11 11:18:53 +0200

    Update

--- dist/js/plugin-twig-doc.js
+++ dist/js/plugin-twig-doc.js
@@ -3,8 +3,12 @@ var TwigDocPlugin = {
 	insert: function(patternData) {
 
 		console.log("trying to insert");
-		console.log(patternData.extraOutput.patternLabPluginKSS);
+		console.log(patternData.extraOutput);
 
-	}
+  },
+
+  setup: function() {
+    console.log('Setup panels');
+  }
 
 }
